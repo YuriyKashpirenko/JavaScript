f526a99e357625d542c50e44425e8b425c50cf4b
YuriyKashpirenko <kashpirenko@gmail.com>
2019-03-09 18:33:15 -0500

    FindMyBeer

--- FindMyBeer/js/script.js
+++ FindMyBeer/js/script.js
@@ -18,7 +18,12 @@ function findBeer(){
       result = JSON.parse(this.responseText);
 
       result.forEach(function(brewery){
-        document.querySelector('#myList').innerHTML = brewery.city;
+        document.querySelector('#br_name').innerHTML = 'Name: ' + brewery.name;
+        document.querySelector('#website').innerHTML = 'Website: ' + brewery.website_url;
+        document.querySelector('#street').innerHTML = 'Street: ' + brewery.street;
+        document.querySelector('#in_city').innerHTML = 'City: ' + brewery.city;
+        document.querySelector('#state').innerHTML = 'State: ' + brewery.state;
+        document.querySelector('#postCode').innerHTML = 'Postal code: ' + brewery.postal_code;
       });
 
     }
@@ -31,5 +36,9 @@ function findBeer(){
 
 }
 
-//  document.querySelector('#error').innerHTML =
-//  'Error.';
+//    <div id = 'br_name'></div>
+//    <div id = 'website'></div>
+//    <div id = 'street'></div>
+//    <div id = 'in_city'></div>
+//    <div id = 'state'></div>  
+//    <div id = 'postCode'></div>  
