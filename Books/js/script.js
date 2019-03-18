056704bdd2ac44cb50fb748a3c04782778ffb3ed
YuriyKashpirenko <kashpirenko@gmail.com>
2019-03-17 20:14:53 -0400

    Books

---
+++ Books/js/script.js
@@ -0,0 +1,43 @@
+'use strict'
+
+//when document(site) is ready/downloaded 
+//if there was a click on button, then execute findBook function
+$(document).ready(function(){
+  $("button").click(findBook);
+});
+
+//function to find books
+function findBook(){
+	//get book from input
+    let book = getBook();
+    //send request to url
+    let urlLink = createURL(book);
+    sendRequest(urlLink);
+}
+
+//function get book name from search input
+function getBook(){
+    let book = $('#bookName').val();
+    return book;
+}
+
+//function to create url link
+function createURL(book){
+    return 'https://www.googleapis.com/books/v1/volumes?q=${book}';
+}
+
+//function to show result, list of book items
+function showBooks(items){
+    let books = items;
+    console.log(books);
+}
+
+//function to send request and get response 
+function sendRequest(urlLink){
+     $.ajax({
+         type:'GET',
+         url:urlLink, 
+         success: showBooks,
+         error: function(){console.log('failed');}
+     });
+ }
